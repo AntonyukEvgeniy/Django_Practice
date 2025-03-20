c0f687c6a962f5eb89e529b088220d33ad41ac5a
Антонюк Евгений <38914583+AntonyukEvgeniy@users.noreply.github.com>
2025-03-20 19:18:13 +0300

    add AbstractUser

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import CustomUser
+from django.utils.translation import gettext_lazy as _
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('username', 'first_name', 'last_name', 'avatar', 'phone_number', 'country')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.email
+
+
 from django.db import models
 
 # Create your models here.
