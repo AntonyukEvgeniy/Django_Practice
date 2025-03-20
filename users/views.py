bc32be6b8bee9bdcd60a4bb1d92015c76ce17b07
Антонюк Евгений <38914583+AntonyukEvgeniy@users.noreply.github.com>
2025-03-20 21:53:19 +0300

    add user register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
